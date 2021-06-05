0dde5358df6f8d3149bf3a7aabf6069e0f4f36b2
glorixz <zxgloria@gmail.com>
2021-06-05 16:17:42 -0600

    init

---
+++ src/main/java/com/example/cornucopia/CornucopiaApplication.java
@@ -0,0 +1,13 @@
+package com.example.cornucopia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CornucopiaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CornucopiaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cornucopia/CornucopiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cornucopia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CornucopiaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
