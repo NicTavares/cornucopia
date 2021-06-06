47bb7c7937780b08a10d77bf19943e35104a72b7
NicTavares <nic.tavares22@gmail.com>
2021-06-05 21:24:51 -0700

    m

---
+++ src/main/java/com/example/models/Equipment.java
@@ -0,0 +1,17 @@
+package com.example.models;
+
+public class Equipment {
+    private String name;
+    public Equipment(String name) {
+        this.name = name;
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
