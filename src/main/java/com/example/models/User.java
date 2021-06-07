9d5fa605976f0af9bbeda0e3f537ba3e2e6d43f0
glorixz <zxgloria@gmail.com>
2021-06-06 19:50:27 -0600

    Administrator, Users, and Course SQL added

--- src/main/java/com/example/models/User.java
+++ src/main/java/com/example/models/User.java
@@ -6,6 +6,7 @@ public class User {
     private final int UUID;
     private final Date birthday;
     private final String email;
+    private final String username;
     private final String name;
     private final String password;
     private final String city;
@@ -13,23 +14,19 @@ public class User {
 
     public User(int UUID, Date birthday,
                 String email,
+                String username,
                 String name,
                 String password,
                 String city,
                 String postalCode) {
 
         this.UUID = UUID;
-
         this.birthday = birthday;
-
         this.email = email;
-
+        this.username = username;
         this.name = name;
-
         this.password = password;
-
         this.city = city;
-
         this.postalCode = postalCode;
     }
 
@@ -47,6 +44,10 @@ public class User {
 
     public String getEmail() {
         return email;
+    }
+
+    public String getUsername() {
+        return username;
     }
 
     public String getName() {
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,6 +1,45 @@
 USE cpsc_304;
 
-DROP TABLE IF EXISTS equipment;
-CREATE TABLE equipment(
+DROP TABLE IF EXISTS Course;
+DROP TABLE IF EXISTS Equipment;
+DROP TABLE IF EXISTS Administrator;
+DROP TABLE IF EXISTS Users;
+
+CREATE TABLE Equipment(
 	name VARCHAR(255) PRIMARY KEY
 );
+
+CREATE TABLE Users (
+       UUID INT PRIMARY KEY,
+       birthday DATE,
+       email VARCHAR(255) NOT NULL,
+       username VARCHAR(255) NOT NULL,
+       name VARCHAR(255) NOT NULL,
+       password VARCHAR(255) NOT NULL,
+       UNIQUE (email),
+       UNIQUE (username)
+);
+
+CREATE TABLE Administrator (
+	admin_UUID INT PRIMARY KEY,
+	admin_level VARCHAR(255), 
+	FOREIGN KEY (admin_UUID) references Users(UUID)
+		ON DELETE CASCADE
+);
+
+CREATE TABLE Course (
+	course_ID INT PRIMARY KEY, 
+	text LONGTEXT, 
+	course_length INT, 
+	course_name VARCHAR(255) NOT NULL, 
+	requirement_name VARCHAR(255), 
+	creator_UUID INT,
+	UNIQUE (course_name),
+	FOREIGN KEY (requirement_name) REFERENCES Equipment(name)
+		ON DELETE SET NULL,
+	FOREIGN KEY (creator_UUID) REFERENCES Administrator(admin_UUID)
+		ON DELETE SET NULL
+);
+
+
+
