c6ebaa8651dc692255a29a1cb762e9d5167be67d
yuangao <plateau451@gmail.com>
2021-06-12 23:45:52 -0700

    feat auth admin

--- src/main/java/com/example/DAO/AdministratorDAO.java
+++ src/main/java/com/example/DAO/AdministratorDAO.java
@@ -1,20 +1,23 @@
 package com.example.DAO;
 
 import com.example.models.Administrator;
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.logging.Logger;
 
+@Component
 public class AdministratorDAO implements DAO<Administrator>{
+    @Autowired
+    JdbcTemplate jdbcTemplate;
 
-    private JdbcTemplate jdbcTemplate;
-    private static final Logger log = (Logger) LoggerFactory.getLogger(EquipmentDAO.class);
-
+    private static final Logger log = LoggerFactory.getLogger(AdministratorDAO.class);
     RowMapper<Administrator> rowMapper = (rs, rowNum) -> {
         Administrator ad = new Administrator(
                 rs.getInt("UUID"),
@@ -66,5 +69,18 @@ public class AdministratorDAO implements DAO<Administrator>{
     public void delete(String id) {
         String sql = "DELETE FROM Administrator WHERE UUID = ?";
         int update = jdbcTemplate.update(sql, id, rowMapper);
+    }
+
+
+
+    public Optional<String> getPasswordByUUID(int UUID) {
+        String sql = "SELECT password FROM Administrator WHERE UUID = ?";
+        String u = null;
+        try{
+            u = jdbcTemplate.queryForObject(sql, String.class, UUID);
+        }catch(DataAccessException e) {
+            System.out.println(e.getMessage());
+        }
+        return Optional.ofNullable(u);
     }
 }
