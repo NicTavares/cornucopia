e92f4fbf9842c82e0bb2a0d95746582e0a36d976
NicTavares <nic.tavares22@gmail.com>
2021-06-14 12:26:41 -0700

    more database access functions2

--- src/main/java/com/example/models/RecipeEquipment.java
+++ src/main/java/com/example/models/RecipeEquipment.java
@@ -1,2 +1,22 @@
-package com.example.models;public class RecipeEquipment {
+package com.example.models;
+
+public class RecipeEquipment {
+    private int UUID;
+    private String name;
+
+    public int getUUID() {
+        return UUID;
+    }
+
+    public void setUUID(int UUID) {
+        this.UUID = UUID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
--- src/main/java/com/example/models/RecipeTag.java
+++ src/main/java/com/example/models/RecipeTag.java
@@ -1,2 +1,22 @@
-package com.example.models;public class RecipeTag {
+package com.example.models;
+
+public class RecipeTag {
+    private int UUID;
+    private String name;
+
+    public int getUUID() {
+        return UUID;
+    }
+
+    public void setUUID(int UUID) {
+        this.UUID = UUID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
