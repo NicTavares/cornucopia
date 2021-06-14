a5d2476372ab5ea887e90f8fb113149a1097a136
glorixz <zxgloria@gmail.com>
2021-06-14 15:53:21 -0600

    added recipeHas data, implemented equipment, tags, ingredients, technique tables

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -49,7 +49,6 @@ VALUES  (11111, 1, '12345','admin+1@gmail.com'),
 		(11113, 1, '12345', 'admin+3@gmail.com'),
 		(11114, 2, '12345','admin+4@gmail.com'),
 		(11115, 3, '12345', 'admin+5@gmail.com');
-
 		
 INSERT INTO UsrFavouriteRecipe(usrUUID,recipeUUID)
 VALUES  ( 11111,1),
@@ -57,6 +56,36 @@ VALUES  ( 11111,1),
 		( 11113,3),
 		( 11114,4),
 		( 11115,5);
+
+INSERT INTO RecipeHasTag(UUID, name)
+VALUES  (00001, 'easy'),
+        (00002, 'healthy'),
+        (00004, 'vegan'),
+        (00004, 'desserts'),
+        (00005, 'japanese');
+
+INSERT INTO RecipeHasIngredient(UUID, name)
+VALUES  (00001, 'chicken'),
+        (00002, 'fish'),
+        (00003, 'egg'),
+        (00003, 'beef'),
+        (00004, 'potato'),
+        (00005, 'beef');
+
+INSERT INTO RecipeHasEquipment(UUID, name)
+VALUES  (00001,'knife'),
+        (00002,'deboning knife'),
+        (00005,'microwave'),
+        (00002,'non-stick pan'),
+        (00004,'non-stick pan'),
+        (00003,'electric kettle');
+
+INSERT INTO RecipeHasTechnique(UUID, name)
+VALUES  (00001,'knife skills'),
+        (00001,'deboning'),
+        (00001,'steaming a fish'),
+        (00002,'sous-vide'),
+        (00002,'tea brewing');
 
 -- INSERT INTO Picture(recipeUUID, pictureTitle, userUUID, filepath)
 -- VALUES  (00001, 'Cover', 11111, './Pictures/cover11111.png'),
@@ -78,19 +107,20 @@ VALUES  (10011, 'Knives are great, hold them with your hand...', 10, 'How to use
 		(10013, 'Press the buttons...', 15, 'How to operate a microwave', 'microwave', 11113),
 		(10014, 'Don''t use metal spatula...', 5, 'How to use a non-stick pan', 'non-stick pan', 11111),
 		(10015, 'Water at 80 degrees...', 5, 'How to brew green tea', 'electric kettle', 11112);
-INSERT INTO UsrHasIngredient(usrUUID, name, quantity)
-VALUES (11114, "fish", 1),
-(11113, "egg", 8),
-(11115, "potato", 3),
-(11112, "chicken", 2),
-(11112, "potato", 4);
 
-INSERT INTO UsrRateRecipe (usrUUID, recipeUUID, score) VALUES
-(11111, 1,7),
-(11114, 2,5),
-(11114, 3,7),
-(11112, 4,9),
-(11113, 5,8);
+INSERT INTO UsrHasIngredient(usrUUID, name, quantity)
+VALUES  (11114, "fish", 1),
+        (11113, "egg", 8),
+        (11115, "potato", 3),
+        (11112, "chicken", 2),
+        (11112, "potato", 4);
+
+INSERT INTO UsrRateRecipe (usrUUID, recipeUUID, score)
+VALUES  (11111, 1,7),
+        (11114, 2,5),
+        (11114, 3,7),
+        (11112, 4,9),
+        (11113, 5,8);
 
 INSERT INTO Comment(recipeUUID, commentNumber, text, authorUUID)
 VALUES  (1, 1, 'nice', 11111),
